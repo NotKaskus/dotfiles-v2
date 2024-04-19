27cfe39e24304bccb0041bdd80c736ea07a8acf0
Paul Andrew <75168528+NotKaskus@users.noreply.github.com>
2024-04-19 04:44:21 +0000

    Update logic when reloading the shell

--- lets-go.sh
+++ lets-go.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # If not already set, specify dotfiles destination directory and source repo
 DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Documents/config/dotfiles}"
 DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/NotKaskus/dotfiles-v2.git}"
@@ -13,15 +15,14 @@ Be sure you've read and understood the what will be applied.\033[0m\n"
 echo -e "\033[1;35mRunning pre-installation script...\033[0m"
 bash <(curl -s https://raw.githubusercontent.com/NotKaskus/dotfiles-v2/main/scripts/installation/pre-install.sh)
 
-# Restart shell
-echo -e "\033[1;35mRestarting shell...\033[0m"
-exec $SHELL
-
 # If dotfiles not yet present then clone
 if [[ ! -d "$DOTFILES_DIR" ]]; then
   mkdir -p "${DOTFILES_DIR}" && \
   git clone --recursive ${DOTFILES_REPO} ${DOTFILES_DIR}
 fi
+
+# Reload shell without killing the session
+source ~/.bashrc
 
 # Execute setup or update script
 cd "${DOTFILES_DIR}" && \
